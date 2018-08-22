e50216680ffd33f853a1045353155d14285727a7
AlexanderPokidin <a.pokidin@gmail.com>
2018-08-22 17:34:36 +0300

    Initial commit

---
+++ app/src/main/java/com/pokidin/a/weatherapplication/Place.java
@@ -0,0 +1,41 @@
+package com.pokidin.a.weatherapplication;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Place {
+
+
+    public static Place newInstance(){
+        return new Place();
+    }
+
+    public LatLng getCoord() {
+        return coord;
+    }
+
+    public void setCoord(LatLng coord) {
+        this.coord = coord;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    private LatLng coord;
+    private Double lat;
+    private Double lng;
+
+
+}
